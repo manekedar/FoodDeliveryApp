e49db0ba9e715fc2f7ed453d4fbdeab957971294
manekedar <kedarmane321@gmail.com>
2025-05-31 17:30:09 +0530

    changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // Fix for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
