b833cccd013e3b56fadecc588aa9eecb355af3d9
manekedar <kedarmane321@gmail.com>
2025-06-04 16:46:40 +0530

    final commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import Orders from './pages/Orders/Orders'
 
 const App = () => {
 
-   const url = "http://localhost:4000"
+   const url = "https://food-delivery-admin-app.vercel.app"
   return (
     <div>
        <Navbar/>
